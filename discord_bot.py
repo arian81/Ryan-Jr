0117ff16435eb6a179615893693a706dd8f33296
arian81 <35879206+arian81@users.noreply.github.com>
2022-06-26 20:22:50 -0400

    added new admin features + youtube tracking

--- discord_bot.py
+++ discord_bot.py
@@ -3,11 +3,14 @@ import discord
 from discord.ext import commands
 import re
 import os
+import asyncio
 from dotenv import load_dotenv
 from database import *
+from youtube import check_new_video
 
 
 load_dotenv()
+
 
 def bot_start():
     intents = discord.Intents.default()
@@ -16,44 +19,63 @@ def bot_start():
 
     @bot.event
     async def on_ready():
-        print(f'We have logged in as {bot.user}')
+        print(f"We have logged in as {bot.user}")
+        while True:
+            if check_new_video():
+                await bot.get_guild(979116054221586463).get_channel(
+                    979116054875889776
+                ).send("Dr(pending) Moore has uploaded a new lecture, check it out!")
+            await asyncio.sleep(1800)
 
     @bot.event
     async def on_member_join(member):
-        await member.send('Welcome to 1XC3 server. Use the /verify example@mcmaster.ca command to verify your email address.')
+        await member.send(
+            "Welcome to 1XC3 server. Use the /verify example@mcmaster.ca command to verify your email address."
+        )
 
     @bot.command(
         help="Verify your McMaster email address",
-        brief="Verify your McMaster email address"
+        brief="Verify your McMaster email address",
     )
     async def verify(message, email_addr):
         if message.guild:
             await message.message.delete()
-            await message.channel.send("Please use the bot's private chat for verifying your account to prevent leaking information")
+            await message.channel.send(
+                "Please use the bot's private chat for verifying your account to prevent leaking information"
+            )
         if re.match(r".*@mcmaster.ca", email_addr):
             if check_email(email_addr):
                 if check_verify(email_addr):
-                    await message.author.send("You're already verified. Have fun in the server :)")
+                    await message.author.send(
+                        "You're already verified. Have fun in the server :)"
+                    )
                 else:
                     gen_token(email_addr)
                     save_discord_id(email_addr, str(message.author.id))
-                    await message.author.send("An email with subject line '1XC3 Discord Server Email Verification' has been sent to the email address provided with activation token you require to activate your account.\nPlease check your spam folder as well as McMaster anti spam system tags emails from non McMaster domains sometimes.")
-                    await message.author.send("Use the command /token 'token sent to email' to verify your account ")
+                    await message.author.send(
+                        "An email with subject line '1XC3 Discord Server Email Verification' has been sent to the email address provided with activation token you require to activate your account.\nPlease check your spam folder as well as McMaster anti spam system tags emails from non McMaster domains sometimes."
+                    )
+                    await message.author.send(
+                        "Use the command /token 'token sent to email' to verify your account "
+                    )
             else:
-                await message.author.send("Your email is not in registered students database. Contact <@!412279988541456387> for help.")
+                await message.author.send(
+                    "Your email is not in registered students database. Contact <@!412279988541456387> for help."
+                )
         else:
             await message.author.send("You need to provide a valid McMaster email.")
 
     @bot.command(
-        help="Send token for verification",
-        brief="Send token for verification"
+        help="Send token for verification", brief="Send token for verification"
     )
     async def token(message, token):
         if check_token(message.author.id, token):
             await addrole(message)
             set_verify(message.author.id)
         else:
-            await message.author.send("That's not a valid token. Your token might've expired. Try again with the /verify command to generate a new token.")
+            await message.author.send(
+                "That's not a valid token. Your token might've expired. Try again with the /verify command to generate a new token."
+            )
 
     async def addrole(message):
         user = message.author
@@ -63,13 +85,39 @@ def bot_start():
             if i.id == user.id:
                 proper_user = i
         await proper_user.add_roles(role)
-        await message.channel.send(f"You are now a verified student. Enjoy the server :)")
+        await message.channel.send(
+            f"You are now a verified student. Enjoy the server :)"
+        )
+
+    # Admin commands
     @bot.command()
-    #if password is correct, query the database and add the user to the server
     async def add(message, email):
         if message.author.id == int(os.getenv("ADMIN_ID")):
             add_user(email)
             await message.channel.send("User added to database.")
+        else:
+            await message.channel.send("Your not authorized to use this command.")
+
+    @bot.command()
+    # if the message author is the admin, check if user exists in the database
+    async def check(message, email):
+        if message.author.id == int(os.getenv("ADMIN_ID")):
+            if check_email(email):
+                await message.channel.send("User exists in database.")
+            else:
+                await message.channel.send("User does not exist in database.")
+        else:
+            await message.channel.send("Your not authorized to use this command.")
+
+    @bot.command()
+    # if the message author is the admin, check if user exists in the database and delete them
+    async def delete(message, email):
+        if message.author.id == int(os.getenv("ADMIN_ID")):
+            if check_email(email):
+                del_user(email)
+                await message.channel.send("User deleted from database.")
+            else:
+                await message.channel.send("User does not exist in database.")
         else:
             await message.channel.send("Your not authorized to use this command.")
 
---
+++ youtube.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import *
+import chromedriver_autoinstaller
+
+chromedriver_autoinstaller.install()
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome("chromedriver", options=chrome_options)
+
+
+def check_new_video():
+    driver.get("https://www.youtube.com/channel/UC7gXXzu2D5rSu3NH4DJw9cw/videos")
+    driver.implicitly_wait(3)
+
+    videos = driver.find_elements(by=By.ID, value="video-title")
+    for i in videos:
+        if "1XC3" in i.text:
+            latest_video = i.get_attribute("href")
+            break
+
+    with open("latest_video.txt", "r") as f:
+        previous_video = f.read().strip("\n")
+    with open("latest_video.txt", "w") as f:
+        f.write(latest_video)
+
+    return previous_video != latest_video
